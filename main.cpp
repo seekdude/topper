5206a20ca8f7ac7d77233139b95921e75e95c2b6
seekdude <rohan.nandu358@gmail.com>
2024-06-01 08:44:50 +0545

    window function

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+class window{
+    private:
+        int height;
+        int width;
+    public:
+        void display(string a){
+            
+        }
+};
